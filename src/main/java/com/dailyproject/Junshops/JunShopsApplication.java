498013d1fd69bf483339acb701f802e62c41e847
Jun Seo <112793862+junseo85@users.noreply.github.com>
2026-02-24 00:06:25 -0600

    Adding styles.css in frontend folder and updating

--- src/main/java/com/dailyproject/Junshops/JunShopsApplication.java
+++ src/main/java/com/dailyproject/Junshops/JunShopsApplication.java
@@ -1,10 +1,13 @@
 package com.dailyproject.Junshops;
 
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JunShopsApplication {
+@CssImport("./themes/junshops/styles.css")
+public class JunShopsApplication  implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JunShopsApplication.class, args);
